481aa4aa75456e6557d395ced90ffc091d00fd27
priyanandavaram <priyanandavaram1993@gmail.com>
2025-12-06 22:11:33 +0000

    cahnge

--- src/BookLendingSolution/LambdaEntryPoint.cs
+++ src/BookLendingSolution/LambdaEntryPoint.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace BookLendingSolution
 {
-    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    public class LambdaEntryPoint : APIGatewayProxyFunction
     {
         protected override void Init(IWebHostBuilder builder)
         {
